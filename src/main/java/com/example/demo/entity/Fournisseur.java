2362f4d729bc20f09ef8d10dc239adc9ac0fe5b9
ilyass bourass <ilyass.b.1992@gmail.com>
2025-10-29 12:15:56 +0100

    GD-6 Mise en place l'architecture du projet

---
+++ src/main/java/com/example/demo/entity/Fournisseur.java
@@ -0,0 +1,14 @@
+package com.example.demo.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Fournisseur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
