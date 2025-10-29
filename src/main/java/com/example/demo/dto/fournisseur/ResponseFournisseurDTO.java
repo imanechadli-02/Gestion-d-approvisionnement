ad98a1fb9266fd3e98cf293e1a7ea8e8a6ed7689
ilyass bourass <ilyass.b.1992@gmail.com>
2025-10-29 12:08:44 +0100

    GD-6 Mise en place l'architecture du projet

---
+++ src/main/java/com/example/demo/dto/fournisseur/ResponseFournisseurDTO.java
@@ -0,0 +1,4 @@
+package com.example.demo.dto.fournisseur;
+
+public class ResponseFournisseurDTO {
+}
--- src/main/java/com/example/demo/enums/StatutCommande.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.enums;
-
-public enum StatutCommande {
-    EN_ATTENTE,
-    VALIDÉE,
-    LIVRÉE,
-    ANNULÉE
-}
-
