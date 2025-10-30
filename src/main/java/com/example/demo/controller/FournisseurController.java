4337a42e026b782fb01f934f9dbc5849da294709
ilyass bourass <ilyass.b.1992@gmail.com>
2025-10-30 21:34:30 +0100

    GD-7 Création du contrôleur Fournisseur avec validation et affichage des fournisseurs

--- src/main/java/com/example/demo/controller/FournisseurController.java
+++ src/main/java/com/example/demo/controller/FournisseurController.java
@@ -1,4 +1,34 @@
 package com.example.demo.controller;
 
+
+import com.example.demo.dto.fournisseur.RequestFournisseurDTO;
+import com.example.demo.dto.fournisseur.ResponseFournisseurDTO;
+import com.example.demo.service.FournisseurService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/fournisseur")
+@RequiredArgsConstructor
+
 public class FournisseurController {
+    private final FournisseurService fournisseurService;
+
+    @PostMapping
+    public ResponseEntity<ResponseFournisseurDTO> createFournisseur(
+            @Valid @RequestBody RequestFournisseurDTO requestdto){
+        ResponseFournisseurDTO response = fournisseurService.createFournisseur(requestdto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResponseFournisseurDTO>>  getALLFournisseur(){
+        List<ResponseFournisseurDTO> fournisseursDTO = fournisseurService.findAll();
+        return new ResponseEntity<>(fournisseursDTO, HttpStatus.OK);
+    }
 }
