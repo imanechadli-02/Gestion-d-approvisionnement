f8b416e70e316e5125af0126ac1a4d304c6cbb6b
ilyass bourass <ilyass.b.1992@gmail.com>
2025-10-30 11:18:19 +0100

    GD-5 Créer l'entitie fournisseur

--- src/main/java/com/example/demo/entity/Fournisseur.java
+++ src/main/java/com/example/demo/entity/Fournisseur.java
@@ -1,14 +1,47 @@
 package com.example.demo.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "fournisseur")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Fournisseur {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "La raison social du fournisseur est obligatoire")
+    private String raison_sociale;
+
+    @NotBlank(message = "l'adress est obligatoire")
+    private String adresse;
+
+    @NotBlank(message = "personne_contact est obligatoire")
+    private String personne_contact;
+
+    @NotBlank(message = "le email est obligatoire")
+    @Email(message = "email non valide")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "le telephone est obligatoire")
+    private String telephone;
+
+    @NotBlank(message = "ville est obligatoire")
+    private String ville;
+
+    @NotBlank(message = "ice est obligatoire")
+    @Column(nullable=false)
+    private String ice;
+
 }
