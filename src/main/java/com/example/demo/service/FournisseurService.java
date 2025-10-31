6e7bfaf3f816f6d0f8f2a2dd4ccec764e936a4a9
ilyass bourass <ilyass.b.1992@gmail.com>
2025-10-31 14:15:32 +0100

    GD-8 Implémentation des fonctionnalités d'affichage, mise à jour et suppression d'un fournisseur

--- src/main/java/com/example/demo/service/FournisseurService.java
+++ src/main/java/com/example/demo/service/FournisseurService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.dto.fournisseur.RequestFournisseurDTO;
 import com.example.demo.dto.fournisseur.ResponseFournisseurDTO;
 import com.example.demo.entity.Fournisseur;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.mapper.FournisseurMapper;
 import com.example.demo.repository.FournisseurRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -34,5 +36,34 @@ public class FournisseurService {
         return fournisseurRepository.findAll()
                 .stream().map(fournisseurMapper::toResponseDTO).toList();
     }
+
+    @Transactional(readOnly = true)
+    public ResponseFournisseurDTO findById(Long id) {
+        Fournisseur fournisseur = fournisseurRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Fournisseur non trouvé avec id : " + id));
+
+        return fournisseurMapper.toResponseDTO(fournisseur);
+    }
+
+    @Transactional
+    public ResponseFournisseurDTO update(Long id, RequestFournisseurDTO requestDTO) {
+        Fournisseur fournisseur = fournisseurRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Fournisseur non trouvé avec id : " + id));
+
+        fournisseurMapper.updateEntityFromDTO(requestDTO, fournisseur);
+
+        Fournisseur updatedFournisseur = fournisseurRepository.save(fournisseur);
+
+        return fournisseurMapper.toResponseDTO(updatedFournisseur);
+    }
+
+    @Transactional
+    public void deleteFournisseur(Long id) {
+        if (!fournisseurRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Fournisseur non trouvé avec id : " + id);
+        }
+        fournisseurRepository.deleteById(id);
+    }
+
 }
 
