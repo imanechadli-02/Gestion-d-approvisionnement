4e9a62d679c1ce1b4671d4bf2058baf50ce88493
imane <imanechadli2001@gmail.com>
2025-11-10 13:46:55 +0100

    GD-50 mettre a jour le diagram de classe

--- src/main/java/com/example/demo/repository/ProduitRepository.java
+++ src/main/java/com/example/demo/repository/ProduitRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
-    Produit findByReference(String reference);
 }
